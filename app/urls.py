10eb4970c3be3caa193f97b66f3a2a5b1549f79b
Walter Pereira Da Silva <waltersilva5@outlook.com>
2021-03-03 16:50:39 -0300

    rotas alteradas

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from .views.views import *
 urlpatterns = [
+    # index
     path('', index, name='index'),
     path('relatorio', relatorio, name='relatorio'),
 
+    # cadastros
     path('cadastros', cadastros, name='cadastros'),
     path('cadastros/cadastrar_veiculo',
          cadastrar_veiculo, name='cadastrar_veiculo'),
@@ -13,6 +15,8 @@ urlpatterns = [
          apagar_veiculo, name='apagar_veiculo'),
     path('cadastros/cadastrar_servico',
          cadastrar_servico, name='cadastrar_servico'),
+
+    # abastecimentos
     path('abastecimentos', abastecimentos, name='abastecimentos'),
     path('abastecimentos/por_veiculo', abastecimentos_por_veiculo,
          name="abastecimentos_por_veiculo"),
@@ -20,11 +24,15 @@ urlpatterns = [
          cadastrar_abastecimento, name="cadastrar_abastecimento"),
     path('abastecimentos/apagar_abastecimento',
          apagar_abastecimento, name="apagar_abastecimento"),
+
+    # serviços
     path('servicos', servicos, name="servicos"),
     path('servicos/por_veiculo', servicos_por_veiculo,
          name="servicos_por_veiculo"),
     path('servicos/apagar_servico', apagar_servico,
          name="apagar_servico"),
+
+    # trocas de oleo
     path('trocas_de_oleo', trocas_de_oleo, name="trocas_de_oleo"),
     path('trocas_de_oleo/nova_troca_de_oleo',
          nova_troca_de_oleo, name="nova_troca_de_oleo"),
